45f0571f720145e577f71187457867b2ac0d6fda
harshraoka <81157043+harshraoka@users.noreply.github.com>
2023-02-27 19:22:34 +0530

    Feat: Added score system

--- quizapp/src/pages/Quiz.jsx
+++ quizapp/src/pages/Quiz.jsx
@@ -7,7 +7,20 @@ import Questions from '../data/Question'
 function Quiz() {
   const [index, setindex] = useState(0);
   const [isEnded, setisEnded] = useState(false);
+  const [points, setpoints] = useState(0);
   function NextQuestion() {
+    var myCheckbox = document.getElementsByName("myCheckbox");
+    Array.prototype.forEach.call(myCheckbox,function(el){
+      if(el.checked){
+        if(el.id===Questions[index].ans){
+          setpoints(points+1);
+          
+        }
+        el.checked=false;
+        
+      
+    }
+    });
     if (index < Questions.length - 1) {
       setindex(index + 1);
     }
@@ -15,6 +28,15 @@ function Quiz() {
       setisEnded(true);
     }
   }
+
+  // function Onlyoneselect(str){
+  //   var myCheckbox = document.getElementsByName("myCheckbox");
+  //   Array.prototype.forEach.call(myCheckbox,function(el){
+  //     if(el.id!=str){
+  //     el.checked=false;
+  //   }
+  //   });
+  // }
   return (
 
 
@@ -23,10 +45,13 @@ function Quiz() {
 
       <h1>Question Number {index}</h1>
       <h4>{Questions[index].prompt}</h4>
-      <input type="radio" />{Questions[index].A}
-      <input type="radio" />{Questions[index].B}
-      <input type="radio" />{Questions[index].C}
-      <input type="radio" />{Questions[index].D}
+      <h1>Score =  {points}</h1>
+      <input type='radio' name='myCheckbox' id='A' />{Questions[index].A}
+      <input type='radio' name='myCheckbox'  id='B' />{Questions[index].B}
+      <input type='radio' name='myCheckbox'  id='B' />{Questions[index].C}
+      <input type='radio' name='myCheckbox'  id='B' />{Questions[index].D}
+      
+
       <button disabled={isEnded} onClick={NextQuestion}>Next Question</button>
 
 
